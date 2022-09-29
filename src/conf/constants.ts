dee4b2cfa1cd7c2d9c136d9d91ff0004ba8c9b1f
Yaroslav Makarov <slideryo@gmail.com>
2022-09-29 14:08:53 +0500

    ga fixed

--- src/conf/constants.ts
+++ src/conf/constants.ts
@@ -2,6 +2,7 @@ export const isProd = process.env.REACT_APP_PRODUCTION === 'true';
 export const baseUrl = process.env.REACT_APP_BASE_URL || '';
 export const apiKey = process.env.REACT_APP_API_KEY || '';
 export const coinIcon = process.env.REACT_APP_CDN_ICONS_KEY || '';
+export const GA_MEASUREMENT_ID = process.env.REACT_APP_GA4_ID || '';
 
 export const crutchCors = [
   '/liquidity-providers-distribution.json',
